c2906d84edf7fc292ccfb378eaa8b20c07565348
aae2079 <aae2079@nyu.edu>
2025-11-22 09:20:06 -0500

    Restructured project

---
+++ include/RectangularMembrane.h
@@ -0,0 +1,12 @@
+#ifdef RECTANGULAR_MEMBRANE_H
+#define RECTANGULAR_MEMBRANE_H
+
+
+class RectangularMembrane {
+    public:
+        RectangularMembrane();
+        ~RectangularMembrane();
+        
+    private:
+
+}
