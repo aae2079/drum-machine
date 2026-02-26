d5fbd10b59f984caaade8a53aa3a6118eb189d1b
Aaron <129892722+aae2079@users.noreply.github.com>
2026-02-26 17:58:49 +0000

    Updated build scripts

--- clean_build.sh
+++ clean_build.sh
@@ -1,7 +1,3 @@
-# Source - https://stackoverflow.com/a
-# Posted by dogbane, modified by community. See post 'Timeline' for change history
-# Retrieved 2025-11-22, License - CC BY-SA 4.0
-
 #!/usr/bin/env bash
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
@@ -9,5 +5,6 @@ BUILD_DIR="$SCRIPT_DIR/build"
 
 cd "$BUILD_DIR" || { echo "Build directory not found!"; exit 1; }
 rm -rf ./*
-cmake ..
-make 
+cmake .. -DCMAKE_VERBOSE_MAKEFILE=ON -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_COMPILER=/usr/bin/g++ >> build.log 2>&1
+cmake --build . >> build.log 2>&1
+echo "Build completed successfully. Logs are available in build/build.log"
