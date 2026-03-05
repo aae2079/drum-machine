7ea5767f386a722b86191c954925e82f53c1660d
Aaron <129892722+aae2079@users.noreply.github.com>
2026-03-05 21:05:00 +0000

    Save work

--- modelling/octave/testAudio.m
+++ modelling/octave/testAudio.m
@@ -1,9 +1,29 @@
 %Main code
-function [hdr, audio] =testAudio(filepath)
+function [hdr, audio] = testAudio(filepath,varargin)
+
+    if (length(varargin) < 2)
+        disp('Usage: [hdr, audio] = testAudio(filepath[string], plot-audio[bool], play-audio[bool])');
+        return;
+    end
+    
+    plots = varargin{1};
+    plays = varargin{2};
 
     [audio,fs] = audioread(filepath);
     hdr = audioinfo(filepath);
-    %keyboard;
+
+    if plots
+        t = (0:length(audio)-1)/fs;
+        figure;
+        plot(t,audio);
+        xlabel('Time (s)');
+        ylabel('Amplitude');
+        title('Audio Signal');
+    end
+
+    if plays
+        sound(audio, fs);
+    end
 end
 
 
