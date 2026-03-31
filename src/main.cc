7f8ec444677317f578f4fb402fcdb2a542645eca
aae2079 <aae2079@nyu.edu>
2026-03-30 21:05:49 -0400

    Frontend class is done...

--- src/main.cc
+++ src/main.cc
@@ -76,7 +76,7 @@ int main(void) {
 		proj  = glm::mat4(1.0f);
 
 		model = glm::rotate(model, glm::radians(rotation), glm::vec3(0.0f, 1.0f, 0.0f));
-		view  = glm::translate(view, glm::vec3(0.0f, -0.5f, -3.5f));
+		view  = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.5f));
 		view  = glm::rotate(view, glm::radians(tilt), glm::vec3(1.0f, 0.0f, 0.0f));
 		proj  = glm::perspective(glm::radians(45.0f), (float)WIDTH / HEIGHT, 2.0f, 100.0f);
 
