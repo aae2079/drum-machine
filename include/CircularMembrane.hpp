0d9594391ae7a8cf56ac1764c43e4c8b5afea6f9
aae2079 <aae2079@nyu.edu>
2026-04-05 22:35:00 -0400

    Added M_PI for real this time

--- include/CircularMembrane.hpp
+++ include/CircularMembrane.hpp
@@ -8,8 +8,8 @@
 #include "simDefs.hpp"
 #include "audioDefs.hpp"
 
-#if defined(_WIN32) || defined(_WIN64)
-    #define M_PI 3.14159265358979323846
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
 #endif
 
 class CircularMembrane {
