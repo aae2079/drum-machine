91a386b650aad97bbcdeaa79e881147217387cdd
Aaron <129892722+aae2079@users.noreply.github.com>
2026-05-01 20:26:50 +0000

    save work

--- include/JsonParser.hpp
+++ include/JsonParser.hpp
@@ -1,4 +1,12 @@
 #ifndef DRUM_MACHINE_JSON_PARSER_H
 #define DRUM_MACHINE_JSON_PARSER_H
+
 #include <string>
 #include "simDefs.hpp"
+#include "rapidjson/document.h"
+
+class JsonParser {
+public:
+    static bool parseJsonSettings(const std::string& filename, Params& params) {};
+};
+#endif
